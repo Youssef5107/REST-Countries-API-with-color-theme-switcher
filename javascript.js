1691821b75767f2719e1a47d67b90b03547a9691
Youssef <ybelalmarzouk@gmail.com>
2024-01-29 00:02:40 +0200

    feat:get cards data

--- javascript.js
+++ javascript.js
@@ -1,4 +1,34 @@
 async function get() {
 	const response = await fetch("data.json");
 	const data = await response.json();
+
+
+	getInnerCards(data);
+
+
 }
+
+function getInnerCards(data) {
+	var card = document.querySelector("#card-container");
+
+	var content = "";
+	for (i = 0; i < data.length; i++) {
+		content = content + `
+				<div class="card">
+					<img class="img-card">
+					<div class="card-details-area">
+						<div class="country-name">${data[i].name}</div>
+						<div class="card-detail">${data[i].population}</div>
+						<div class="card-detail">${data[i].region}</div>
+						<div class="card-detail">${data[i].capital}</div>
+					</div>
+				</div>		
+		`
+	}
+
+	card.innerHTML = content;
+}
+
+
+get()
+
